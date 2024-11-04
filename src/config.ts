be39eb6a2ebfaeb222f0964d734a286d023fbfcd
MathPSantos <mathsantos.dev@gmail.com>
2024-11-04 15:06:51 +0100

    feat: initialize arara.school website

---
+++ src/config.ts
@@ -0,0 +1,93 @@
+export const COURSES = [
+  {
+    title: "Design Conversacional",
+    description:
+      "Conheça o mercado e as habilidades para atuar como designer conversacional, princípios, arquitetura da informação, design sprint, escrita, usabilidade e mais!",
+    href: "/cursos/design-conversacional",
+    background: "#167ffc",
+    text: "#ffffff",
+  },
+  {
+    title: "UX Writing e Conteúdo Conversacionais",
+    description:
+      "Conheça as habilidades para escrever para interfaces conversacionais, princípios básicos e avançados, linguagem simples, métricas, curadoria e mais!",
+    href: "/cursos/ux-writting-conteudo-conversionais",
+    background: "#595bd4",
+    text: "#ffffff",
+  },
+  {
+    title: "UX Writing para Chatbots",
+    description:
+      "Conheça os fundamentos de UX Writing aplicados em Chatbots, bem como, ensinamentos e boas práticas sobre como aplicar em projetos.",
+    href: "/cursos/us-writting-chatbots",
+    background: "#ffcb2f",
+    text: "#000000",
+  },
+  {
+    title: "Vocabulário Controlado para Chatbots",
+    description:
+      "Aprenda sobre o universo dos vocabulários controlados e como utilizar esse conhecimento no processo de criação de uma interface conversacional.",
+    href: "/cursos/vocabulario-controlado-chatbots",
+    background: "#ff6b88",
+    text: "#000000",
+  },
+  {
+    title: "Design Centrado no Usuário",
+    description:
+      "O curso aborda temas como: história, boas práticas de design, cases de implementação, Design Thinking, pesquisas com usuárias, formas de testes no DCU e apresentação de um desafio com foco em portfólio.",
+    href: "/cursos/design-centrado-usuario",
+    background: "#fe9526",
+    text: "#000000",
+  },
+  {
+    title: "Speech Analytics",
+    description:
+      "Aprenda os conceitos sobre a tecnologia de Speech Analytics, quais as técnicas e melhores praticas para utilização de ferramentas, parar se obter o máximo de valor a partir da voz do cliente.",
+    href: "/cursos/speech-analytics",
+    background: "#3cabdb",
+    text: "#000000",
+  },
+  {
+    title: "Métricas Conversacionais",
+    description:
+      "Aprenda os conceitos fundamentais relacionados às métricas conversacionais, desde o momento apropriado para começar a considerar seu uso até a aplicação prática de indicadores de desempenho.",
+    href: "/cursos/metricas-conversacionais",
+    background: "#333545",
+    text: "#ffffff",
+  },
+  {
+    title: "ChatGPT para UX Writing",
+    description:
+      "Aprenda técnicas de escrita de prompts (comandos para ChatGPT) associadas a boas práticas de conteúdo para criar entregáveis de UX Writing e impactar positivamente a experiência da pessoa usuária.",
+    href: "/cursos/chatgpt-para-ux-writting",
+    background: "#53d86a",
+    text: "#000000",
+  },
+  {
+    title: "Localização para UX Writers",
+    description:
+      "Aprenda o que é Localização (sua importância, exemplos bons e ruins), atuação do UX Writer como Especialista em Localização, internacionalização (i18n), tradução (t9n), transcreação, emojis e Localização e muito mais.",
+    href: "/cursos/localizacao-para-ux-writters",
+    background: "#fd3d39",
+    text: "#ffffff",
+  },
+];
+
+export const KNOWLEDGE_SPRINTS = [
+  {
+    title: "Conhecendo VUI Design",
+    description:
+      'Descubra o mundo do "VUI Design"! Esta trilha aborda irá te ajudar na criação de interfaces interativas e sonoras, capacitando-se para se destacar no mercado conversacional. Impulsione sua carreira na era da tecnologia comunicativa!',
+    href: "/trilhas/conhecendo-vui-design",
+    background: "#53d86a",
+    text: "#000000",
+  },
+  {
+    title: "Fundamentos para Interfaces Conversacionais",
+    description:
+      "Nesta trilha, descubra o Futuro da Comunicação a partir de seus fundamentos básicos! Conheça a base do UX Design e Writing para Chatbots em nossos módulos de Introdução ao Mercado Conversacional e muito mais.",
+    href: "/trilhas/fundamentos-para-interfaces-conversacionais",
+    background: "#53d86a",
+    text: "#000000",
+  },
+];
