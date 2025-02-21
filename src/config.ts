b598cafb3d2b1bb145c1fd09ad9bd10c89d45573
MathPSantos <mathsantos.dev@gmail.com>
2025-02-21 17:54:49 -0300

    feat: finish arara course page

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,217 @@
 export const COURSES = [
   {
+    id: "design-conversacional",
     title: "Design Conversacional",
     description:
       "Conheça o mercado e as habilidades para atuar como designer conversacional, princípios, arquitetura da informação, design sprint, escrita, usabilidade e mais!",
     href: "/cursos/design-conversacional",
     background: "#167ffc",
     text: "#ffffff",
+    cta: {
+      description: `
+        <p>Aprenda a projetar experiências conversacionais e as habilidades chaves para atuar com o papel de designer conversacional na sua empresa.</p>
+        <p>Entenda como mapear problemas, identificar oportunidades, criar fluxos e documentações, realizar testes de usabilidade e facilitar a criação de um produto conversacional através de uma sprint.</p>
+        <p>
+          <strong>Você vai aprender tudo isso e mais neste curso inédito no Brasil facilitado por um time incrível de especialistas!</strong>
+        </p>
+      `,
+    },
+    price: {
+      installment: "R$33,25",
+    },
+    reasons: `
+      <p>
+        <b>Se você está buscando conhecimentos para idealizar, influenciar ou conduzir na construção de experiências conversacionais, seja para chatbots, voicebots ou multimodais, este curso é para você.</b>
+      </p>
+
+      <p>Aprenda as habilidades chave para guiar você ou o seu time no processo de criação, o que inclui fundamentos em design conversacional, conteúdos conversacionais, ux design, ux writing, arquitetura da informação, acessibilidade, curadoria, usabilidade, design sprint e mais.</p>
+
+      <p>O mercado brasileiro anda aquecido na procura por soluções conversacionais. Por isso, aumentou a procura por profissionais e especializações voltadas para este mercado. <b>Aprenda com quem sabe.</b></p>
+    `,
+    learning: [
+      {
+        title: "Introdução ao mercado conversacional",
+        content:
+          "Conheça as oportunidades e desafios para projetar experiências conversacionais. Neste módulo, vamos conhecer os princípios e conceitos básicos que podem ajudar você a guiar o seu time ou empresa no processo de criação de um projeto conversacional.",
+      },
+      {
+        title: "Fundamentos em Design Conversacional",
+        content:
+          "Conheça os principais fundamentos relacionados às interfaces conversacionais e como aplicar cada um deles no seu próximo projeto conversacional.",
+      },
+      {
+        title: "Arquitetura da informação",
+        content:
+          "Entenda como surgiu o conceito, os primeiros estudos e como a arquitetura da informação pode influenciar no desenvolvimento e criação das interfaces conversacionais.",
+      },
+      {
+        title: "Conteúdo e UX Writing",
+        content:
+          "Descubra a mágica de transformar textos em conversas para interfaces conversacionais. Veja como a construção do conteúdo pode influenciar na personalidade das conversas, na escolha das palavras e além.",
+      },
+      {
+        title: "Prototipação",
+        content:
+          "A prototipação é uma etapa fundamental no desenvolvimento de uma ideia. Aprenda a construir protótipos simples e complexos para testar e validar suas ideias.",
+      },
+      {
+        title: "Inteligência Artificial e Curadoria",
+        content:
+          "Entenda como a inteligência artificial pode ser utilizada como um atalho conversacional nos atendimentos",
+      },
+      {
+        title: "Usabilidade",
+        content:
+          "Ter contato com as noções básicas para planejar, produzir, conduzir, analisar e apresentar resultados de pesquisa em testes de usabilidade.",
+      },
+      {
+        title: "Sound Design",
+        content:
+          "O objetivo deste módulo é ensinar&nbsp;como funciona o processo de criação de Sound Design para interfaces conversacionais. Inclui conteúdos sobre vocabulários do áudio, dicas de direção e gravação,&nbsp;atmosferas sonoras, e boas práticas para trabalhar com áudio nas interfaces conversacionais.",
+      },
+      {
+        title: "Aspectos Comunicativos",
+        content:
+          "O objetivo deste módulo é apresentar e discutir alguns elementos da interação humano-computador baseados na comunicação, utilizados em projetos de interfaces conversacionais. Estudaremos algumas estratégias que podem ser usadas por projetistas desse tipo de interface para demonstrar suas funcionalidades aos usuários na prática, bem como desafios de projeto e limitações das metodologias de avaliação existentes, que são baseadas nos elementos estudados.",
+      },
+    ],
+    team: [
+      {
+        image: "",
+        name: "Bernardo Mattos",
+        role: "Líder de Experiência do Usuário (UX Lead)",
+        linkedin: "https://www.linkedin.com/in/bemattos/",
+      },
+    ],
+    skills: {
+      title:
+        "Todas as habilidades e conhecimentos que você precisa para atuar na área",
+      items: [
+        {
+          image: "",
+          title: "Comece do zero",
+          description:
+            "Aprenda os principais conteúdos relacionados a área e desenvolva o seu conhecimento",
+        },
+      ],
+    },
+    faq: [
+      {
+        title:
+          "Quanto tempo preciso dedicar para acompanhar a trilha? É ao vivo ou gravado?",
+        content: `
+          <p>Como as aulas são gravadas, você pode aprender de acordo com o seu ritmo e dedicar o tempo que você achar necessário.</p>
+
+          <p>Nossa sugestão para ajudar no seu aprendizado é você dedicar 2-4h de estudos por semana.</p>
+
+          <p>Em caso durante as aulas, só postar na nossa comunidade ou comentar nas aulas, outras pessoas vão estar aprendendo com você também 😉</p>
+
+          <p>De toda forma, vá sem pressa e aprenda no seu tempo. Por isso, oferecemos diferentes opções de assinatura que permite você assinar e ter acesso ao material por mais ou menos tempo.</p>
+        `,
+      },
+      {
+        title: "A trilha oferece certificado?",
+        content: `
+          <p>Sim.</p>
+
+<p>Emitimos um certificado de conclusão no final do curso para as pessoas que completarem a prova final com aproveitamento acima de 70%.</p>
+
+<p>Ao concluir todo o curso e tirar uma nota acima de 70%, você receberá o seu certificado.</p>
+
+<p>Você poderá se apresentar como uma pessoa profissional certificada na área. 😉</p>
+        `,
+      },
+      {
+        title: "E se eu não gostar da experiência?",
+        content: `
+          <p>Ao se matricular no curso, você terá até 7 dias para nos dizer se gostou ou não da experiência.</p>
+
+<p>Caso não tenha gostado, você pode solicitar seu dinheiro de volta.</p>
+        `,
+      },
+      {
+        title: "Tem algum pré-requisito?",
+        content: `
+          <p>Precisa apenas curiosidade e interesse em aprender.</p>
+
+<p>Além disso, é necessário ter algum dispositivo, computador ou smartphone, com acesso a internet para poder acessar as aulas.</p>
+
+<p>Fora isso, não há nenhum pré-requisito ou necessidade de experiência prévia. Vamos ajudar você a aprender.</p>
+        `,
+      },
+      {
+        title: "Tem alguma atividade?",
+        content: `
+          <p>Cada módulo tem um conjunto de atividades para ajudar você a fixar os seus aprendizados.</p>
+
+<p>São atividades simples e baseadas no conteúdo apresentado ao longo das aulas.</p>
+        `,
+      },
+      {
+        title: "Posso falar com especialistas?",
+        content: `
+          <p>Sim, mas por enquanto não estamos oferecendo mentorias ou horários dedicados com especialistas.</p>
+
+<p>De toda forma, quando você se matricula no nosso curso, você ganha acesso a nossa comunidade com demais estudantes e especialistas.</p>
+
+<p>Você pode compartilhar seus aprendizados, dúvidas e interagir com outras pessoas como se fosse um grupo no Facebook ou LinkedIn.</p>
+        `,
+      },
+      {
+        title: "Posso usar outro meio de pagamento?",
+        content: `
+          <p>Sim, é possível pagar utilizando outro meio de pagamento ou quantidade de parcelas no cartão de crédito.</p>
+
+<p>Além de pagamentos via cartão de crédito, também podemos receber via depósitos bancários, transferência pix ou boleto.</p>
+
+<p>Por favor, entre em contato informando a sua necessidade por email: <a href="mailto:contato@arara.school" class="underline text-blue-500 font-semibold">contato@arara.school</a>. Vamos entrar em contato em até 2 dias úteis.</p>
+        `,
+      },
+      {
+        title: "Vocês emitem nota fiscal?",
+        content: `
+          <p>Sim. Emitimos nota fiscal para pessoas físicas ou empresas.</p>
+
+<p>Para isso, por favor, informe a sua necessidade por email: <a href="mailto:contato@arara.school" class="underline text-blue-500 font-semibold">contato@arara.school</a> </p>
+        `,
+      },
+      {
+        title: "Tem desconto para grupos? Como funciona?",
+        content: `
+          <p>Sim, oferecemos descontos progressivos para grupos de 3 ou mais pessoas.</p>
+
+<p>Indicado para empresas, times inteiros, ou grupos de pessoas que querem se matricular com desconto.</p>
+
+<p>Em caso de dúvidas, fale com a gente no <a href="mailto:contato@arara.school" class="underline text-blue-500 font-semibold">contato@arara.school</a> e informe a quantidade de licenças desejadas.</p>
+        `,
+      },
+      {
+        title: "Tenho outras dúvidas!",
+        content: `
+          <p>Para outras dúvidas, por favor, fale com a gente no <a href="mailto:contato@arara.school" class="underline text-blue-500 font-semibold">contato@arara.school</a>.</p>
+        `,
+      },
+    ],
   },
   {
+    id: "ux-writing-e-conteudos-conversacionais",
     title: "UX Writing e Conteúdo Conversacionais",
     description:
       "Conheça as habilidades para escrever para interfaces conversacionais, princípios básicos e avançados, linguagem simples, métricas, curadoria e mais!",
     href: "/cursos/ux-writting-conteudo-conversionais",
     background: "#595bd4",
     text: "#ffffff",
+    cta: {
+      description: `
+        <p>
+          <strong>Aprenda as principais habilidades relacionadas ao universo da escrita para texto, fala e multimodal, com foco e conversacional.</strong>
+        </p>
+        <p>Neste curso inédito no Brasil, conheça as boas práticas para escrita para interfaces conversacionais, princípios básicos e avançados em UX Writing e conteúdos voltados para a conversas.</p>
+      `,
+    },
   },
   {
+    id: "ux-writing-para-chatbots",
     title: "UX Writing para Chatbots",
     description:
       "Conheça os fundamentos de UX Writing aplicados em Chatbots, bem como, ensinamentos e boas práticas sobre como aplicar em projetos.",
@@ -24,6 +220,7 @@ export const COURSES = [
     text: "#000000",
   },
   {
+    id: "vocabulario-controlado-para-chatbots",
     title: "Vocabulário Controlado para Chatbots",
     description:
       "Aprenda sobre o universo dos vocabulários controlados e como utilizar esse conhecimento no processo de criação de uma interface conversacional.",
@@ -32,6 +229,7 @@ export const COURSES = [
     text: "#000000",
   },
   {
+    id: "design-centrado-no-usuario",
     title: "Design Centrado no Usuário",
     description:
       "O curso aborda temas como: história, boas práticas de design, cases de implementação, Design Thinking, pesquisas com usuárias, formas de testes no DCU e apresentação de um desafio com foco em portfólio.",
@@ -40,6 +238,7 @@ export const COURSES = [
     text: "#000000",
   },
   {
+    id: "speech-analytics",
     title: "Speech Analytics",
     description:
       "Aprenda os conceitos sobre a tecnologia de Speech Analytics, quais as técnicas e melhores praticas para utilização de ferramentas, parar se obter o máximo de valor a partir da voz do cliente.",
@@ -48,6 +247,7 @@ export const COURSES = [
     text: "#000000",
   },
   {
+    id: "metricas-conversacionais",
     title: "Métricas Conversacionais",
     description:
       "Aprenda os conceitos fundamentais relacionados às métricas conversacionais, desde o momento apropriado para começar a considerar seu uso até a aplicação prática de indicadores de desempenho.",
@@ -56,6 +256,7 @@ export const COURSES = [
     text: "#ffffff",
   },
   {
+    id: "chatgpt-para-ux-writing",
     title: "ChatGPT para UX Writing",
     description:
       "Aprenda técnicas de escrita de prompts (comandos para ChatGPT) associadas a boas práticas de conteúdo para criar entregáveis de UX Writing e impactar positivamente a experiência da pessoa usuária.",
@@ -64,6 +265,7 @@ export const COURSES = [
     text: "#000000",
   },
   {
+    id: "workshop-localizacao-para-uxwriters",
     title: "Localização para UX Writers",
     description:
       "Aprenda o que é Localização (sua importância, exemplos bons e ruins), atuação do UX Writer como Especialista em Localização, internacionalização (i18n), tradução (t9n), transcreação, emojis e Localização e muito mais.",
